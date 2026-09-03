ba82e098be3a719aaf17f49b009153df952c36d2
Dylan Manuel <dylan.manuel@att.net>
2026-09-03 16:13:45 -0500

    docs: started docs

--- src/config.rs
+++ src/config.rs
@@ -12,15 +12,21 @@ static DIRS: LazyLock<ProjectDirs> = LazyLock::new(|| {
 });
 static DATA_DIR: LazyLock<&Path> = LazyLock::new(|| DIRS.data_local_dir());
 
+/// Where the `.onnx` graphs and `.onnx_data` weights are stored.
 pub static ONNX_DIR: LazyLock<RwLock<PathBuf>> =
     LazyLock::new(|| RwLock::new(DATA_DIR.join("onnx")));
+/// Path to the Chatterbox-Turbo tokenizer
 pub static TOKENIZER_PATH: LazyLock<RwLock<PathBuf>> =
     LazyLock::new(|| RwLock::new(DATA_DIR.join("tokenizer.json")));
 
+/// Maximum number of model artifacts to download in parallel
 #[cfg(feature = "download")]
 pub static MAX_CONCURRENT_DOWNLOADS: LazyLock<RwLock<usize>> = LazyLock::new(|| RwLock::new(4));
+/// If download progress should be displayed via progress bar
 #[cfg(feature = "download")]
 pub static SHOW_DOWNLOAD_PROGRESS: LazyLock<RwLock<bool>> = LazyLock::new(|| RwLock::new(true));
+/// HuggingFace authentication token. Populating this will make downloads more faster. Defaults to the environment's
+/// `HF_TOKEN` variable.
 #[cfg(feature = "download")]
 pub static HF_TOKEN: LazyLock<RwLock<Option<String>>> =
     LazyLock::new(|| RwLock::new(env::var("HF_TOKEN").ok()));
